804bec36d7bbaa251b42dec40502a71d9f9e2690
sanni prasad <prasadsunny1@gmail.com>
2021-04-26 00:20:21 +0530

    Added gridview for viewing notes

--- lib/all_notes/all_notes_page.dart
+++ lib/all_notes/all_notes_page.dart
@@ -1,15 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:very_good_notes/create_notes/create_notes_page.dart';
 import 'package:very_good_notes/signin_page.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class AllNotesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Notes'),
+        title: const Text('Very Good Notes'),
         actions: [
           IconButton(
               icon: const Icon(Icons.logout),
@@ -25,7 +27,7 @@ class AllNotesPage extends StatelessWidget {
         onPressed: () {
           var route = MaterialPageRoute(
             builder: (context) {
-              return CreateNotesPage();
+              return const CreateNotesPage();
             },
           );
 
@@ -34,14 +36,110 @@ class AllNotesPage extends StatelessWidget {
         child: const Icon(Icons.add),
       ),
       body: Container(
-        child: ListView(
-          children: [
-            const ListTile(
-              title: Text('Title'),
-              subtitle: Text('Subtitle'),
-              tileColor: Colors.yellow,
-            )
-          ],
+        child: UserInformation(),
+      ),
+    );
+  }
+}
+
+class UserInformation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final notes = FirebaseFirestore.instance.collection('notes');
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: notes.snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Center(child: Text('Something went wrong'));
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        return StaggeredGridView.countBuilder(
+          primary: false,
+          crossAxisCount: 4,
+          mainAxisSpacing: 4,
+          crossAxisSpacing: 4,
+          padding: const EdgeInsets.all(8),
+          itemCount: snapshot.data?.size ?? 0,
+          itemBuilder: (context, index) {
+            var document = snapshot.data?.docs[index];
+            return NoteTile(
+              title: document?.data()['title'] ?? '',
+              body: document?.data()['note'] ?? '',
+              documentRefId: document?.id,
+              onPressed: (documentRefId) {
+                // open the edit page with the doc ref
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return CreateNotesPage(documentId: document?.id);
+                    },
+                  ),
+                );
+              },
+            );
+          },
+          staggeredTileBuilder: (index) => const StaggeredTile.fit(2),
+        );
+      },
+    );
+  }
+}
+
+class NoteTile extends StatelessWidget {
+  const NoteTile(
+      {Key? key,
+      this.title,
+      this.body,
+      this.documentRefId,
+      required this.onPressed})
+      : super(key: key);
+
+  final String? title;
+  final String? body;
+  final String? documentRefId;
+  final Function(String documentRefId) onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (documentRefId != null) {
+          onPressed(documentRefId!);
+        }
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.black45,
+            width: 1.0,
+          ),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                title ?? '',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              const SizedBox(
+                height: 4,
+              ),
+              Text(
+                body ?? '',
+                style: Theme.of(context)
+                    .textTheme
+                    .subtitle1
+                    ?.copyWith(color: Colors.grey),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/create_notes/create_notes_page.dart
+++ lib/create_notes/create_notes_page.dart
@@ -1,81 +1,161 @@
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class CreateNotesPage extends StatelessWidget {
+class CreateNotesPage extends StatefulWidget {
+  final String? documentId;
+
+  const CreateNotesPage({Key? key, this.documentId}) : super(key: key);
+  @override
+  _CreateNotesPageState createState() => _CreateNotesPageState();
+}
+
+class _CreateNotesPageState extends State<CreateNotesPage> {
+  final titleTextEditingController = TextEditingController();
+  final noteTextEditingController = TextEditingController();
+  final fireStore = FirebaseFirestore.instance;
+
+  String currentNoteDocRef = '';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          children: [
-            TextField(
-              controller: TextEditingController(),
-              decoration: const InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Title',
-              ),
-              onChanged: (value) {
-                _addOrUpdateNote(value, '');
-              },
-            ),
-            TextField(
-              controller: TextEditingController(),
-              decoration: const InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Note',
-              ),
-              onChanged: (value) {
-                _addOrUpdateNote('', value);
-              },
-            ),
-            const Spacer(),
-            Row(
-              children: [
-                IconButton(
-                    icon: const Icon(Icons.add_a_photo),
-                    onPressed: () {
-                      //TODO: pick and image and upload it to storage
-                      //get the link and attach it to the storage
-                    }),
-                const Spacer(),
-                IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () {
-                    // delete the note
-                  },
+    return WillPopScope(
+      onWillPop: () async {
+        var title = titleTextEditingController.text;
+        var note = noteTextEditingController.text;
+        await _addOrUpdateNote(title, note);
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              TextField(
+                controller: titleTextEditingController,
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Title',
                 ),
-              ],
-            ),
-          ],
+                textCapitalization: TextCapitalization.sentences,
+                onChanged: (value) {
+                  // _addOrUpdateNote(value, '');
+                },
+              ),
+              TextField(
+                controller: noteTextEditingController,
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Note',
+                ),
+                onChanged: (value) {
+                  // _addOrUpdateNote('', value);
+                },
+              ),
+              const Spacer(),
+              Row(
+                children: [
+                  IconButton(
+                      icon: const Icon(Icons.add_a_photo),
+                      onPressed: () {
+                        //TODO: pick and image and upload it to storage
+                        //get the link and attach it to the storage
+                      }),
+                  const Spacer(),
+                  IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () {
+                      // delete the note
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  final fireStore = FirebaseFirestore.instance;
-  String currentNoteDocRef = '';
-  Future<void> _addOrUpdateNote(String title, String note) async {
-    var mapToUpdate = <String, dynamic>{};
-    if (title.isNotEmpty) {
-      mapToUpdate.addAll({'title': title});
-    }
-    if (note.isNotEmpty) {
-      mapToUpdate.addAll({'note': note});
-    }
+  @override
+  void initState() {
+    super.initState();
+    if (widget.documentId != null && widget.documentId!.isNotEmpty) {
+      currentNoteDocRef = widget.documentId!;
 
+      FirebaseFirestore.instance
+          .collection('notes')
+          .doc(currentNoteDocRef)
+          .get()
+          .then((doc) {
+        titleTextEditingController.text = doc.data()?['title'] ?? '';
+        noteTextEditingController.text = doc.data()?['note'] ?? '';
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    // get values and store to firebase
+    //
+  }
+
+  Future<bool?> _addOrUpdateNote(String title, String note) async {
+    var mapToUpdate = <String, dynamic>{'title': title, 'note': note};
+    var isAllEmpty = mapToUpdate.values
+        .cast<String>()
+        .every((String element) => element.isEmpty);
+    if (isAllEmpty) {
+      var shouldDelete = await showDialog<bool>(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            title: const Text('Delete the note?'),
+            children: [
+              SimpleDialogOption(
+                child: ElevatedButton(
+                  child: const Text('Delete'),
+                  onPressed: () {
+                    Navigator.pop(context, true);
+                  },
+                ),
+              ),
+              SimpleDialogOption(
+                child: ElevatedButton(
+                  child: const Text('Cancel'),
+                  onPressed: () {
+                    Navigator.pop(context, false);
+                  },
+                ),
+              ),
+            ],
+          );
+        },
+      );
+      if (shouldDelete != null &&
+          shouldDelete &&
+          currentNoteDocRef.isNotEmpty) {
+        await fireStore.collection('notes').doc(currentNoteDocRef).delete();
+      }
+      return false;
+    }
     if (currentNoteDocRef.isNotEmpty) {
       // Update the existing Doc
       await fireStore
           .collection('notes')
           .doc(currentNoteDocRef)
-          .update(mapToUpdate);
+          .update(mapToUpdate)
+          .onError((error, stackTrace) {
+        log(error.toString(), stackTrace: stackTrace);
+      });
     } else {
       // create a new note
-
+      print('Create a new note');
       var docRef = await fireStore.collection('notes').add(mapToUpdate);
       currentNoteDocRef = docRef.id;
     }
+    return true;
   }
 }
